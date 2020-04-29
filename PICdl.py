6ada592059e88abf86a0f0e3707c92d92c908a99
harold aaron <haroldaaron0987@gmail.com>
2020-04-29 09:20:42 +0800

    added folder creation just modify to your liking

--- PICdl.py
+++ PICdl.py
@@ -1,18 +1,27 @@
 from selenium import webdriver
 import urllib.request
 import random
+import os 
 
 username = str(input("Enter username: "))
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("D:/testbot/chromedriver.exe")
 driver.get("https://www.instagram.com/"+username+"/")
 
 images = driver.find_elements_by_class_name("FFVAD")
 
-for image in images[:2]:
-    filename = 'img'+str(random.randint(1,100000))+'.jpg'
-    url = image.get_attribute('src')
-    urllib.request.urlretrieve(url, filename)
+path = r"D:\testbot"
+fname = username
+imagepath =os.path.join(path,fname)
+os.makedirs(imagepath, exist_ok=True)
+os.chdir(imagepath)
+
+if  os.getcwd() == imagepath:  
+    for image in images[:2]:
+        filename = 'img'+str(random.randint(1,100000))+'.jpg'
+        
+        url = image.get_attribute('src')
+        urllib.request.urlretrieve(url, filename)
 
 driver.close()
 driver.quit()
